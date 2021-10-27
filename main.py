55b4f40e95a5ab7eb9ae668a63223158ed24a7f6
Aman Juneja <71624646+amankn@users.noreply.github.com>
2021-10-27 11:11:17 +0000

    even or odd number challenge completed.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
-
+if(number % 2 == 0):
+  print(f" {number} is a even number")
+else:
+  print(f"{number} is odd number")
 
 
